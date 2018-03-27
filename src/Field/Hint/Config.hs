40f91cd16b79623b0a61f9c29d91e23d278ccf0d
Charles Durham <fabricatedmath@gmail.com>
2018-03-26 20:16:32 -0400

    modularized hint descr

--- src/Field/Hint/Config.hs
+++ src/Field/Hint/Config.hs
@@ -1,53 +1,45 @@
-{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Config
-  ( loadConfigFromArgs, Descr(..)
-  , optHintDescr, optLogMultiplier, optFile
-  , breakFileExtension
+module Field.Hint.Config
+  ( loadHintDescrFromArgs
+  , hintDescrOptions
   )
 where
 
-import Control.Arrow ((***))
 import Control.Lens
 import Control.Monad (when)
 
-import Data.Aeson (decode')
+import Data.Aeson (decode', ToJSON(..), FromJSON(..))
 import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy.Char8 as BS
-
 import Data.Either (partitionEithers)
-import Data.Maybe (fromMaybe, listToMaybe)
+import Data.Maybe (listToMaybe)
 
 import System.Console.GetOpt
-import System.Environment (getArgs, getProgName)
 import System.Exit
-import System.IO
 
 import Field
 import Field.Hint
 
-data Descr =
+data Descr a =
   Descr
-  { _optHintDescr :: HintDescr Double
-  , _optLogMultiplier :: Maybe Double
-  , _optFile :: (FilePath, Maybe String)
+  { _optDescr :: HintDescr a
   , _optWriteJSON :: Bool
   , _optStop :: Bool
-  } deriving (Show, Read)
+  } deriving (Show)
 
 makeLenses ''Descr
 
-defaultDescr :: Descr
+defaultDescr :: Num a => Descr a
 defaultDescr =
   Descr
-  { _optHintDescr = defaultHintDescr
-  , _optLogMultiplier = Nothing
-  , _optFile = ("default", Just ".png")
+  { _optDescr = defaultHintDescr
   , _optWriteJSON = False
   , _optStop = False
   }
 
+--TODO: base 4.11 introduced theses into Data.Maybe
 fromLeft :: a -> Either a b -> a
 fromLeft a (Right _) = a
 fromLeft _ (Left a) = a
@@ -56,11 +48,14 @@ fromRight :: b -> Either a b -> b
 fromRight _ (Right b) = b
 fromRight b (Left _) = b
 
-loadConfigFromArgs :: IO Descr
-loadConfigFromArgs =
+loadHintDescrFromArgs
+  :: (ToJSON a, Num a, RealFrac a, Read a, FromJSON a, Show a)
+  => FilePath --out file path
+  -> [String]
+  -> IO (HintDescr a)
+loadHintDescrFromArgs outfp args =
   do
-    args <- getArgs
-    let (actions,a,s) = getOpt RequireOrder options args
+    let (actions,_,_) = getOpt RequireOrder hintDescrOptions args
         (lefts,rights) = partitionEithers actions
     descr <-
       do
@@ -72,36 +67,32 @@ loadConfigFromArgs =
                 mjson <- decode' <$> BS.readFile fp
                 case mjson of
                   Nothing -> die "Failed to read JSON file"
-                  Just json -> pure $ (optHintDescr .~ json) descr
+                  Just json -> pure $ (optDescr .~ json) descr
             Nothing -> pure descr
         foldl (>>=) (pure descr') rights
     when (_optWriteJSON descr) $
       do
-        BS.writeFile (fst (_optFile descr) ++ ".json") $
-          encodePretty (_optHintDescr descr)
+        BS.writeFile (outfp ++ ".json") $
+          encodePretty (_optDescr descr)
         putStrLn "Wrote json file"
     when (_optStop descr) $
       do
         putStrLn "Stopping due to flag"
         exitSuccess
-    return descr
+    return $ _optDescr descr
 
-breakFileExtension :: FilePath -> (FilePath, Maybe String)
-breakFileExtension fp =
-  let
-    (filename,path) =
-      reverse *** reverse $ break (=='/') $ reverse fp
-    (extension,name) =
-      reverse *** (reverse . drop 1) $ break (=='.') $ reverse filename
-  in (path ++ name, if null extension then Nothing else Just extension)
-
-options :: [OptDescr (Either FilePath (Descr -> IO Descr))]
-options =
+hintDescrOptions
+  :: forall a. (RealFrac a, Read a, Show a)
+  => [OptDescr (Either FilePath (Descr a -> IO (Descr a)))]
+hintDescrOptions =
+  let def :: HintDescr a
+      def = defaultHintDescr
+  in
   [ Option "F" []
     (ReqArg
       (\arg ->
          pure (\opt ->
-                 pure $ optHintDescr.hintDescrFS %~
+                 pure $ optDescr.hintDescrFS %~
                  (\a ->
                     Left $ fString .~ arg $ fromLeft defaultCartesian a
                  ) $ opt
@@ -114,7 +105,7 @@ options =
     (ReqArg
       (\arg ->
          pure (\opt ->
-                  pure $ optHintDescr.hintDescrFS %~
+                  pure $ optDescr.hintDescrFS %~
                   (\a ->
                      Left $ gString .~ arg $ fromLeft defaultCartesian a
                   ) $ opt
@@ -126,7 +117,7 @@ options =
   , Option "P" []
     (NoArg
       (pure
-        (\opt -> pure $ optHintDescr.hintDescrFS .~ Right defaultPolar $ opt)
+        (\opt -> pure $ optDescr.hintDescrFS .~ Right defaultPolar $ opt)
       )
     )
     $ unlines $ ["Set to use polar coordinates in terms of r and theta"
@@ -135,7 +126,7 @@ options =
     (ReqArg
       (\arg ->
          pure (\opt ->
-                  pure $ optHintDescr.hintDescrFS %~
+                  pure $ optDescr.hintDescrFS %~
                   (\a ->
                      Right $ rString .~ arg $ fromRight defaultPolar a
                   ) $ opt
@@ -148,7 +139,7 @@ options =
     (ReqArg
       (\arg ->
          pure (\opt ->
-                  pure $ optHintDescr.hintDescrFS %~
+                  pure $ optDescr.hintDescrFS %~
                   (\a ->
                      Right $ rString .~ arg $ fromRight defaultPolar a
                   ) $ opt
@@ -157,71 +148,55 @@ options =
       "String")
     $ unlines $ ["Function String for polar 'theta dot', T (r,theta) = _"
                 ,"Default: " ++ show (defaultPolar ^. tString)]
-  , Option "f" []
-    (ReqArg
-      (\arg -> pure (\opt -> pure $ optFile .~ breakFileExtension arg $ opt))
-      "File")
-    $ unlines $ ["PNG file save name"
-                ,"Default: " ++ show (
-                    let (pre,suf) = defaultDescr ^. optFile
-                    in pre ++ fromMaybe "" suf)]
   , Option "a" ["aa"]
     (ReqArg
       (\arg ->
          pure (\opt ->
-                  pure $ optHintDescr.hintDescrFD.fdAA .~ read arg $ opt
+                  pure $ optDescr.hintDescrFD.fdAA .~ read arg $ opt
               )
       )
       "Int")
     $ unlines $ ["Anti-Aliasing"
                 ,"Default: " ++
-                 show (defaultDescr ^. optHintDescr.hintDescrFD.fdAA)]
+                 show (def ^. hintDescrFD.fdAA :: Int)]
   , Option "r" ["res"]
     (ReqArg
       (\arg ->
-         pure (\opt ->
-                 pure $
-                 optHintDescr.hintDescrFD.fdRes .~ read arg ^. tupleToV2 $ opt
-              )
+         pure
+         (\opt ->
+             pure $ optDescr.hintDescrFD.fdRes .~ read arg ^. tupleToV2 $ opt
+         )
       )
       "(Int,Int)")
     $ unlines $
     ["Resolution of output image, needs quotes in cmd line"
     ,"Default: " ++
-      show (defaultDescr ^. optHintDescr.hintDescrFD.fdRes.v2ToTuple.to show)]
+      show (def ^. hintDescrFD.fdRes.v2ToTuple.to show)]
   , Option "c" ["center"]
     (ReqArg
      (\arg ->
-        pure (\opt ->
-                pure $
-                optHintDescr.hintDescrFD.fdCenter .~ read arg ^. tupleToV2 $ opt
-             )
+        pure
+        (\opt ->
+            pure $ optDescr.hintDescrFD.fdCenter .~ read arg ^. tupleToV2 $ opt
+        )
      )
       "(Double,Double)")
     $ unlines $
     ["Center of field View, needs quotes in cmd line"
     ,"Default: " ++
-     show (defaultDescr ^. optHintDescr.hintDescrFD.fdCenter.v2ToTuple.to show)]
+     show (def ^. hintDescrFD.fdCenter.v2ToTuple.to show)]
   , Option "H" ["height"]
     (ReqArg
      (\arg ->
-        pure (\opt ->
-                pure $ optHintDescr.hintDescrFD.fdHeight .~ read arg $ opt
-             )
+        pure
+        (\opt ->
+            pure $ optDescr.hintDescrFD.fdHeight .~ read arg $ opt
+        )
      )
       "Double")
     $ unlines $ ["Height of field view"
                 ,"Default: " ++
-                 show (defaultDescr ^. optHintDescr.hintDescrFD.fdHeight)]
-  , Option "m" ["multiplier"]
-    (ReqArg
-     (\arg -> pure (\opt -> pure $ optLogMultiplier .~ Just (read arg) $ opt))
-      "Double")
-    $ unlines $
-    ["Multiplier of log function to convert colors,"
-    ,"a higher number means less intensity variability "
-    ,"between low and high magnitude vectors"
-    ,"Default: " ++ show (defaultDescr ^. optLogMultiplier)]
+                 show (def ^. hintDescrFD.fdHeight)]
   , Option "" ["write-json"]
     (NoArg
       (pure (\opt -> pure $ optWriteJSON .~ True $ opt))
@@ -235,12 +210,4 @@ options =
     (NoArg
       (pure (\opt -> pure $ optStop .~ True $ opt))
     ) "Don't generate field image"
-  , Option "h" ["help"]
-    (NoArg
-      (pure (\_ -> do
-          prg <- getProgName
-          hPutStrLn stderr $ usageInfo prg options
-          exitWith ExitSuccess
-      ))
-    ) "Show help"
   ]
